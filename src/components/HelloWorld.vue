44c46721f9a9c101eae428d8952b3029f9f27bdd
Aplex <alexander.dahlberg@student.kyh.se>
2018-05-21 10:05:27 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <p v-for="connected in connections">{{connected}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      connections: []
+    }
+  },
+  sockets: {
+    connectedPlayer: function(data){
+      this.connections = data;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
